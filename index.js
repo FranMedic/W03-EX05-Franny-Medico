e9f33a10fa6dac63eb286df2061174bdbb3a7230
FranMedic <m.francisca.medico@gmail.com>
2021-10-15 11:06:50 +0200

    added buttonEvent

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import Page from "./Page.js";
+import Component from "./Component.js";
 
 const pageBody = document.querySelector("body");
-const mainWeb = new Page(pageBody, "container", "div");
+const mainWeb = new Component(pageBody, "container", "div");
 
 const button = document.querySelector(".button__htmlcreator");
+
+button.addEventListener("click", () => {
+  const generateTag = document.querySelector(".creator").value;
+  const paintTag = new Component(pageBody, "", generateTag);
+});
