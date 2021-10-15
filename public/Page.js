185b3063f2d95e270b0d752cae058ce852ca3c08
FranMedic <m.francisca.medico@gmail.com>
2021-10-15 14:31:00 +0200

    fold public created, and moved all files inside

---
+++ public/Page.js
@@ -0,0 +1,12 @@
+import Component from "./Component.js";
+
+class Page extends Component{
+  constructor(parentElement){
+    super(parentElement);
+    this.element.generateHTML();
+  }
+  const inputText =
+  
+}
+
+export default Page;
